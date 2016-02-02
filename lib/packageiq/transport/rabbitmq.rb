d0c07958f106a42d925fccedc01097a9d2277e56
Eric Olsen <eric@ericolsen.net>
2016-02-02 16:06:23 -0500

    added tls options to rabbitmq transport

--- lib/packageiq/transport/rabbitmq.rb
+++ lib/packageiq/transport/rabbitmq.rb
@@ -4,21 +4,28 @@ module Packageiq
   module Transport
     # rabbitmq transport
     class Rabbitmq
-      attr_accessor :host, :port, :ssl, :vhost, :user, :pass
+      attr_accessor :host, :port, :tls,
+                    :tls_cert, :tls_key, :tls_ca_certificates,
+                    :vhost, :user, :pass
       def initialize(args = {})
-        @host   = args[:host] || '127.0.0.1'
-        @port   = args[:port] || 5672
-        @ssl    = args[:ssl] || false
-        @vhost  = args[:vhost] || '/'
-        @user   = args[:user] || 'guest'
-        @pass   = args[:pass] || 'guest'
+        @host                   = args[:host] || '127.0.0.1'
+        @port                   = args[:port] || 5672
+        @tls                    = args[:tls] || false
+        @tls_cert               = args[:tls_cert] || ''
+        @tls_key                = args[:tls_key] || ''
+        @tls_ca_certificates    = args[:tls_ca_certificates] || []
+        @vhost                  = args[:vhost] || '/'
+        @user                   = args[:user] || 'guest'
+        @pass                   = args[:pass] || 'guest'
       end
 
       attr_reader :conn
       def start
         @conn = Bunny.new(host: host, port: port,
-                          ssl: ssl, vhost: vhost,
-                          user: user, pass: pass)
+                          tls: tls, tls_cert: tls_cert,
+                          tls_key: tls_key,
+                          tls_ca_certificates: tls_ca_certificates,
+                          vhost: vhost, user: user, pass: pass)
         conn.start
       end
 
